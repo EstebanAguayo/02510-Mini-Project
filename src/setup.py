684d4f51b8c93fcf591c07cc4f4b694f4c67998a
EstebanAguayo <s213162@dtu.dk>
2023-04-20 10:52:46 +0200

    Initial commit

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deeplearning approach for sllice Wise segmentation task',
+    author='group06',
+    license='MIT',
+)
